77322123c1917d0f131bf1e7d23ea3ad99d01b14
Sidharth Surya <imsidharthsurya@gmail.com>
2024-01-26 12:45:49 +0530

    passing multiple child

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
-const heading=React.createElement("h1",{id:"head1"},"this is react");
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+  React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement("h1", {}, "This is 1st heading of child1"),React.createElement("h2",{},"This is 2nd heading of child1")]
+  ),React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement("h1", {}, "This is 1st heading of child2"),React.createElement("h2",{},"This is 2nd heading of child2")]
+  )]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
