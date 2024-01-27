a07422132b37065e55c625487682aed6b5ef4992
Sidharth Surya <imsidharthsurya@gmail.com>
2024-01-27 22:03:26 +0530

    jsx & functional component added

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-  React.createElement(
-    "div",
-    { id: "child1" },
-    [React.createElement("h1", {}, "This is 1st and last heading of child1"),React.createElement("h2",{},"This is 2nd heading of child1")]
-  ),React.createElement(
-    "div",
-    { id: "child2" },
-    [React.createElement("h1", {}, "This is 1st heading of child2"),React.createElement("h2",{},"This is 2nd heading of child2")]
-  )]
-);
+// const heading = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   [
+//   React.createElement(
+//     "div",
+//     { id: "child1" },
+//     [React.createElement("h1", {}, "This is 1st and last heading of child1"),React.createElement("h2",{},"This is 2nd heading of child1")]
+//   ),React.createElement(
+//     "div",
+//     { id: "child2" },
+//     [React.createElement("h1", {}, "This is 1st heading of child2"),React.createElement("h2",{},"This is 2nd heading of child2")]
+//   )]
+// );
+
+//react element using jsx
+// const jsxheading=<h1 id="head1">This is jsx heading</h1>
+// console.log(jsxheading)
+
+//functional component
+const Title=()=><h1>Learning React component</h1>;
+const Middle=()=>{
+    return (<div id="container">
+        <Title/>
+        <p>Made by sidharth 27-01-2024</p>
+    </div>)
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Middle/>)
